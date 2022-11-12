50e6b62c9b03d2e8cb23cbc575021e967c9bb1b2
Navodya Himashi Jayasinghe <it19210070@my.sliit.lk>
2022-11-12 17:36:48 +0530

    modelFace

---
+++ face_age_gender_rec/face_recognition/face_rec.py
@@ -0,0 +1,98 @@
+# Import our libaries
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout, \
+    Activation
+from PIL import Image
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, save_img, img_to_array
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+model = Sequential()
+model.add(ZeroPadding2D((1, 1), input_shape=(224, 224, 3)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(Convolution2D(4096, (7, 7), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(4096, (1, 1), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(2622, (1, 1)))
+model.add(Flatten())
+model.add(Activation('softmax'))
+
+model.load_weights('face_age_gender_rec/face_recognition/vgg_face_weights.h5')
+
+
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(224, 224))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+
+def findCosineSimilarity(source_representation, test_representation):
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+
+
+# Our model that outputs the 2,622 embedding vector
+vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+
+epsilon = 0.80
+
+
+def verifyFace(img1, img2):
+    # Get embedding/encoding for face1 and face2
+    img1_representation = vgg_face_descriptor.predict(preprocess_image('blockchain/face_images/%s' % (img1)))[0, :]
+    img2_representation = vgg_face_descriptor.predict(preprocess_image('upload/%s' % (img2)))[0, :]
+
+    # Calculate the cosine similarity between the two embeddings
+    cosine_similarity = findCosineSimilarity(img1_representation, img2_representation)
+
+    print("Cosine similarity: ", cosine_similarity)
+
+    # If similarity score is less than the epsilon threshold
+    if (cosine_similarity < epsilon):
+        print("They are same person")
+        return True
+    else:
+        print("They are not same person!")
+        return False
+
